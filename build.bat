f8e986befba7a8010c4ec04f06c6f040bc2028e6
陈君 <chenjun@xmyanqu.com>
2026-06-25 18:25:54 +0800

    升级环境

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files\Microsoft Visual Studio\2026\Community\VC\Auxiliary\Build\vcvars32.bat"
 cd /d D:\Game\BeiDou\BeiDou-ijl15\ezorsia
 msbuild ezorsia.vcxproj /p:Configuration=Release /p:Platform=Win32
